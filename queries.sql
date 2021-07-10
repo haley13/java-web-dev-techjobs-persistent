2ec440a8c417184285484ee248496035e390bba9
Haley13 <allickhaley@gmail.com>
2021-07-10 14:07:49 -0500

    need to finish query

--- queries.sql
+++ queries.sql
@@ -1,16 +1,17 @@
 ## Part 1: Test it with SQL
-CREATE TABLE job(
+SELECT *
    job_id INT AUTO_INCREMENT PRIMARY KEY,
    employer VARCHAR(255),
    name VARCHAR(255),
    skill VARCHAR(255),
    employer_id INT
-   );
+
 
 ## Part 2: Test it with SQL
-SELECT employer.location
+SELECT employer.name
 FROM employer
 WHERE (location= "St. Louis City");
 ## Part 3: Test it with SQL
+DROP TABLE job
 
 ## Part 4: Test it with SQL
